075cc1fc80334718ef58ae925298ba3f3b2139ec
Ngo <ngomalalibo@yahoo.com>
2021-01-18 22:46:00 +0100

    Deployment to AWS EC2 with Travis CI

--- travis_scripts/launch_build.sh
+++ travis_scripts/launch_build.sh
@@ -1,3 +1,16 @@
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+gem install travis
+sudo rm /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m) >docker-compose
+chmod +x docker-compose
+sudo mv docker-compose /usr/local/bin
 echo "Launching $BUILD_NAME IN AMAZON ECS"
 scp -i all-stock-key-pair.pem docker-compose.yml ubuntu@ec2-3-121-40-225.eu-central-1.compute.amazonaws.com:.
 docker container rm -f $(docker container ls -aq)
