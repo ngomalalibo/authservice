8fd602ab1aaf5450ae8cf9e025c202e6bc0cf35d
Ngo <ngomalalibo@yahoo.com>
2021-01-19 06:45:16 +0100

    Deployment to AWS EC2 with Travis CI

--- travis_scripts/launch_build.sh
+++ travis_scripts/launch_build.sh
@@ -12,7 +12,6 @@ curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compos
 chmod +x docker-compose
 sudo mv docker-compose /usr/local/bin
 echo "Launching $BUILD_NAME IN AMAZON ECS"
-scp -i all-stock-key-pair.pem docker-compose.yml ubuntu@ec2-3-121-40-225.eu-central-1.compute.amazonaws.com:.
 docker container rm -f $(docker container ls -aq)
 docker image rm -f $(docker image ls -aq)
 docker-compose up -d
